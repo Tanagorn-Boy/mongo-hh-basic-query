fc403bc5173947f286c6376200875ce2ce1e6f83
Amornthep <amornthep.tee@gmail.com>
2024-11-15 12:05:19 +0700

    Assignment No SQL Basic

---
+++ exercise-4.js
@@ -0,0 +1,14 @@
+db.pizzaOrders.find({
+  quantity: { $lt: 5 },
+  credit_card_type: "mastercard",
+});
+
+db.pizzaOrders.find({
+  size: "small",
+  $and: [{ quantity: { $gte: 1 } }, { quantity: { $lte: 5 } }],
+});
+
+db.pizzaOrders.find({
+  quantity: { $gt: 10 },
+  credit_card_type: { $not: /mastercard/i },
+});
